6cb856eb055e8acf9e514a7203c60322b86fa1cc
Digvijay Singh <singh.dvjay@gmail.com>
2019-06-20 17:24:22 +0530

    Thu Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const MaintenanceContract = require('./lib/maintenance-contract.js');
+
+module.exports.MaintenanceContract = MaintenanceContract;
+module.exports.contracts = [ MaintenanceContract ];
